0aa0ff7f79e22e316713457cb3a68596449ca0ae
Michal <michalplebanski@gmail.com>
2018-05-11 18:32:19 +0200

    test commit

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-app.use(express.static('assets'));
+app.use('/static', express.static('assets'));
 
-app.get('/', function(req, res) {
-	res.sendFile('./index.html');
+app.get('/cos', function(req, res) {
+	res.sendFile('http://localhost:3000/static/index.html');
 });
 
 app.get('/userForm', function(req, res) {
@@ -12,7 +12,7 @@ app.get('/userForm', function(req, res) {
 		last_name: req.query.last_name
 	};
 	res.send(JSON.stringify(response));
-})
+});
 
 const server = app.listen(3000, 'localhost', function() {
 	const host = server.address().address;
